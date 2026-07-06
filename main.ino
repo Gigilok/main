11b5d4d764c000c1ab52432b6c938d0b180bfa47
Gigilok <cliquefeirago@gmail.com>
2026-07-06 15:54:53 -0300

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,23 @@
+#include "config.h"
+
+// Declarações externas das funções do menu
+extern void setupHardware();
+extern void initMenuSystem();
+extern void drawMenu();
+extern void handleMenu();
+extern void runCurrentFunction();
+
+void setup() {
+  setupHardware();
+  initMenuSystem();
+  drawMenu();
+}
+
+void loop() {
+  if (current_screen == 0) {
+    handleMenu();
+  } else {
+    runCurrentFunction();
+  }
+  delay(10); // Pequeno delay para estabilidade
+}
